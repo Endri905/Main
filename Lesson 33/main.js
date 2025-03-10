bfc6f5914de0252ddb6ce744950004c7d1c1190a
Endri905 <latifajendri905@gmail.com>
2025-03-10 04:10:21 -0700

    A

--- Lesson 33/main.js
+++ Lesson 33/main.js
@@ -1,51 +1,17 @@
 function validation(){
     var name=document.getElementById("name").value;
 
-    var valid_name_regex=/^[A-Za-z]+$/;
-
-    var name=document.getElementById("age").value;
+    var valid_name_regrex=/^[A-Za-z]+$/;
+    
+    var age=document.getElementById("age").value;
 
     var valid_age_regex=/^[0-9]+$/;
 
     var city=document.getElementById("city").value;
 
-    if(!(name.match(valid_name_regex)) || (age.match(valid_age_regex)) || (city == "")){
-
-        if(!(name.match(valid_name_regex))){
-            document.getElementById("name_error").style.visibility="visible";
-            document.getElementById("name").style.borderColor="Red";
-        }
-        else{
-            document.getElementById("name_error").style.visibility="hidden";
-            document.getElementById("name").style.borderColor="black";
-        }
-
-        if(!(age.match(valid_age_regex))){
-            document.getElementById("age_error").style.visibility="visible";
-            document.getElementById("age").style.borderColor="Red";
-        }
-        else{
-            document.getElementById("age_error").style.visibility="hidden";
-            document.getElementById("age").style.borderColor="black";
-        }
-
-        if(city == ""){
-            document.getElementById("city_error").style.visibility="visible";
-            document.getElementById("city").style.borderColor="Red";
-        }
-        else{
-            document.getElementById("city_error").style.visibility="hidden";
-            document.getElementById("city").style.borderColor="black";
-        }
-        return false;
-        
-    }else{
-        document.getElementById("name_error").style.visibility="hidden"
-        document.getElementById("name").style.borderColor="black";
-        document.getElementById("age_error").style.visibility="hidden"
-        document.getElementById("age").style.borderColor="black";
-        document.getElementById("city_error").style.visibility="hidden"
-        document.getElementById("city").style.borderColor="black";
-        return true;
-    }
+    if(!(name.match(valid-name_regex))) || (age.match(valid_age_regex))  ||  (city -- "")[
+        if(!)name.match(valid_name_regex))
+            document.getElementById("name_error").style.visibility="visible;
+        document.getElementById( )
+    ]
 }
